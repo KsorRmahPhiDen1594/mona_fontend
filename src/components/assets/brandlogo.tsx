1a6d69158642092864fccc14128cdc31c86cf90d
Trần Trân Lộc <tran26122003@gmail.com>
2025-07-04 21:29:26 +0700

    Header

--- src/components/assets/brandlogo.tsx
+++ src/components/assets/brandlogo.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@/lib/utils";
 
 const BrandLogo = ({ dark = false, className }: { dark?: boolean; className?: string }) => {
-  const logoSrc = dark ? "/images/pages/home/brands/logo.webp" : "/images/pages/home/brands/logo.webp";
+  const logoSrc = dark ? "https://mona.media/wp-content/uploads/2024/10/media-logo-new-1.png" : "https://mona.media/wp-content/uploads/2024/10/media-logo-new-1.png";
   return (
     <div className={cn(className, "flex items-center gap-2")}>
       <img
         src={logoSrc}
         alt="Brand Logo"
-        className="w-26 h-26 object-contain"
+        className=" w-[380px] object-contain"
       />
     </div>
   );
